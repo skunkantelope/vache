78048c75ed71d29058d71f2752b367d002d8452a
Qian Wang <QiQi@Pony-Studio.local>
2013-09-11 17:03:54 -0700

    Currently enable uploading photos by both camera and photo library. Will use photo library only, since the location information is not available in camera.

--- Berkeley 311/BasicFormViewController.m
+++ Berkeley 311/BasicFormViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "BasicFormViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface BasicFormViewController ()
 
@@ -147,6 +149,55 @@
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
+    
+    // Get the image metadata;
+    UIImagePickerControllerSourceType pickerType = picker.sourceType;
+    if (pickerType == UIImagePickerControllerSourceTypeCamera) {
+        NSDictionary *imageMetadata = [info objectForKey:UIImagePickerControllerMediaMetadata];
+            
+        // Get the assets library
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        ALAssetsLibraryWriteImageCompletionBlock imageWriteCompletionBlock = ^(NSURL *newURL, NSError *error) {
+            if (error) {
+                NSLog(@"Can not save the photo %@.", error);
+            } else {
+                NSLog(@"Wrote image with metadata to Photo Library");
+            }
+        };
+        
+        // save the new image to the camera roll
+        UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        [library writeImageToSavedPhotosAlbum:[image CGImage] metadata:imageMetadata completionBlock:imageWriteCompletionBlock];
+        NSLog(@"metadata: %@", [imageMetadata description]);
+    } else if (pickerType == UIImagePickerControllerSourceTypePhotoLibrary) {
+        // Get the assets library
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        
+        // Enumerate just the photos group by using ALAssetsGroupSavedPhotos.
+        [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+            
+            // Within the group enumeration block, filter to enumerate just photos.
+            [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+            
+            [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:0]
+                                    options:0
+                                 usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
+                                     // The end of the enumeration is signaled by asset == nil.
+                                     if (alAsset) {
+                                    //     ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                                    //     NSDictionary *imageMetadata = [representation metadata];
+                                         
+                                         CLLocation *location = [alAsset valueForProperty:ALAssetPropertyLocation];
+                                         NSDate *date = [alAsset valueForProperty:ALAssetPropertyDate];
+                                         NSLog(@"location %@ and date %@", location, date);
+                                     }
+                                 }];
+        } failureBlock:^(NSError *error) {
+            NSLog(@"Photo is not found");
+        }];
+    }
+    
+    [picker.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
@@ -154,7 +205,7 @@
 }
 
 - (IBAction)sendPhotoEvidence:(id)sender {
-    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Enter manually" destructiveButtonTitle:nil otherButtonTitles:@"Select a photo", @"Take a new photo", nil];
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Enter manually" destructiveButtonTitle:nil otherButtonTitles:@"Select a photo", /*@"Take a new photo",*/ nil];
    // NSLog(@"cancel index %i", sheet.cancelButtonIndex); why is the index is 2? It's supposed to be -1.
     [sheet showInView:(UIView *)sender];
     
